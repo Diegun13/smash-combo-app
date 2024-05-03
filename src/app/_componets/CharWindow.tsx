dcf50792b58de802b3ff7870e09dea2c2a2105be
Diegun13 <63631029+Diegun13@users.noreply.github.com>
2024-05-03 19:31:16 -0400

    appprops

--- src/app/_componets/CharWindow.tsx
+++ src/app/_componets/CharWindow.tsx
@@ -1,4 +1,5 @@
-export default function CharWindow(name:string){
+import { AppProps } from "next/app"
+export default function CharWindow({name}:AppProps){
     return(
         <>
         <p className="bg-rose-500">{name}</p>
